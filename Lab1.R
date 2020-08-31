c60f6fd10d160c62904bf7ae30028d07308a4f00
AliceZheng <alicej.zheng@gmail.com>
2020-08-31 09:30:06 -0400

    lab 1

---
+++ Lab1.R
@@ -0,0 +1,24 @@
+# Exercise 1
+x <- 3
+y <- 15
+
+x + y
+x - y
+x * y
+x / y
+
+
+# Exercise 2
+x <- 2
+y <- 3
+
+x^5 > y^4
+
+# Exercise 3
+v <- c(22, 62, 148, 43, 129)
+sum(v)
+v/sum(v)
+
+# Exercise 4
+v <- c('A', 'T', 'C', 'G')
+sort(v)
